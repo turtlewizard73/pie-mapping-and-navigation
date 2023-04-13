c8a73951f5766ec29d659b0479abb815df7cbba0
turtlewizard <mate.laszlo703@gmail.com>
2023-04-13 23:34:46 +0200

    cylinder test

--- workspace/src/robot_startup/plugins/actor_collisions/ActorCollisionsPlugin.cc
+++ workspace/src/robot_startup/plugins/actor_collisions/ActorCollisionsPlugin.cc
@@ -19,9 +19,13 @@
 
 #include <gazebo/physics/Actor.hh>
 #include <gazebo/physics/CylinderShape.hh>
+#include <gazebo/physics/BoxShape.hh>
+#include "gazebo/physics/Shape.hh"
 #include <gazebo/physics/Collision.hh>
 #include <gazebo/physics/Link.hh>
 #include "ActorCollisionsPlugin.hh"
+
+#include <typeinfo>
 
 using namespace gazebo;
 GZ_REGISTER_MODEL_PLUGIN(ActorCollisionsPlugin)
@@ -35,6 +39,8 @@ ActorCollisionsPlugin::ActorCollisionsPlugin()
 /////////////////////////////////////////////////
 void ActorCollisionsPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
 {
+  std::cout << "Loading ActorCollisionsPlugin" << std::endl;
+  std::cout << "Model name: " << _model->GetName() << std::endl;
   // Get a pointer to the actor
   auto actor = boost::dynamic_pointer_cast<physics::Actor>(_model);
 
@@ -75,36 +81,64 @@ void ActorCollisionsPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
   {
     // Init the links, which in turn enables collisions
     link->Init();
+    std::cout << "_________________________" << std::endl;
+    std::cout << typeid(link).name() << std::endl;
 
-    // if (scaling.empty())
-    //   continue;
+    if (scaling.empty())
+      continue;
 
-    // // Process all the collisions in all the links
-    // for (const auto &collision : link->GetCollisions())
-    // {
-    //   auto name = collision->GetName();
+    // Process all the collisions in all the links
+    for (const auto &collision : link->GetCollisions())
+    {
+      auto name = collision->GetName();
 
-    //   if (scaling.find(name) != scaling.end())
-    //   {
-    //     /*auto boxShape = boost::dynamic_pointer_cast<gazebo::physics::BoxShape>(
-    //         collision->GetShape());
+      auto old_shape = collision->GetShape();
+      std::cout << "___________old_shape______________" << std::endl;
+      std::cout << typeid(old_shape).name() << std::endl;
 
-    //     // Make sure we have a box shape.
-    //     if (boxShape)
-    //       boxShape->SetSize(boxShape->Size() * scaling[name]);*/
+      auto parent = collision->GetShape()->GetParent();
+      std::cout << "___________parent______________" << std::endl;
+      std::cout << typeid(parent).name() << std::endl;
 
-    //     auto cylinderShape = boost::dynamic_pointer_cast<gazebo::physics::CylinderShape>(
-    //         collision->GetShape());
+      gazebo::physics::CylinderShapePtr cylinder_shape(new gazebo::physics::CylinderShape(collision));
+      cylinder_shape->SetSize(0.5, 1.0);
+      // auto shape = boost::dynamic_pointer_cast<gazebo::physics::CylinderShape>(collision->GetShape()->GetParent());
+      std::cout << "New shape type: " << cylinder_shape->GetType() << std::endl;
+      if(cylinder_shape){
 
-    //     if (cylinderShape)
-    //       cylinderShape->SetScale(scaling[name]);
-    //   }
+        std::cout << collision->GetShape()->GetParent() << std::endl;
+        std::cout << cylinder_shape << std::endl;
 
-    //   if (offsets.find(name) != offsets.end())
-    //   {
-    //     collision->SetInitialRelativePose(
-    //         offsets[name] + collision->InitialRelativePose());
-    //   }
-    // }
+        collision->SetShape(cylinder_shape);
+
+        auto new_shape = collision->GetShape();
+        std::cout << "___________new_shape______________" << std::endl;
+        std::cout << typeid(new_shape).name() << std::endl;
+
+      }
+      //TODO collision legyen cylinder
+
+      /*if (scaling.find(name) != scaling.end())
+      {
+        auto boxShape = boost::dynamic_pointer_cast<gazebo::physics::BoxShape>(
+            collision->GetShape());
+
+        // Make sure we have a box shape.
+        if (boxShape)
+          boxShape->SetSize(boxShape->Size() * scaling[name]);
+
+        auto cylinderShape = boost::dynamic_pointer_cast<gazebo::physics::CylinderShape>(
+            collision->GetShape());
+
+        if (cylinderShape)
+          cylinderShape->SetScale(scaling[name]);
+      }
+
+      if (offsets.find(name) != offsets.end())
+      {
+        collision->SetInitialRelativePose(
+            offsets[name] + collision->InitialRelativePose());
+      }*/
+    }
   }
 }
